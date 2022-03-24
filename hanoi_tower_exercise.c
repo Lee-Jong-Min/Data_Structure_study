c36471ba1f601afd1abb1279584fa59e4ce33bd4
JongMin Lee <2314ljm@gmail.com>
2022-03-25 00:06:15 +0900

    hanoi_tower_exercise.c

---
+++ hanoi_tower_exercise.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+void hanoi_tower(int n, char from, char tmp, char to)
+{
+	if(n == 1)
+		printf("원판 1을 %c에서 %c으로 옮긴다.\n",from,to);
+	else
+	{
+		hanoi_tower(n-1,from,to,tmp);
+		printf("원판 %d을 %c에서 %c으로 옯긴다.\n",n,from,to);
+		hanoi_tower(n-1,tmp,from,to);
+	}
+}
+
+void main()
+{
+	clock_t start,finish;
+	double duration;
+
+	start = clock();
+	hanoi_tower(15,'A','B','C');
+	finish = clock();
+	duration = (double)(finish - start);
+	printf("%lf 클럭입니다.",duration);
+
+}
